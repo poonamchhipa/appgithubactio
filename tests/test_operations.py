4e29a6ce771d4b69a59a496cead70985facae504
poonamchhipa <poonamchhipa4@gmail.com>
2025-01-17 11:07:04 +0530

    added

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,7 +3,7 @@ def test_add() -> None:
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     
-def test_sub() - > None:
+def test_sub() -> None:
     assert sub(5, 3) == 2
     assert sub(4, 3) == 1
     assert sub(3, 3) == 0
